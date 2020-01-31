ee828ae6ea54e00e6838392e5ffd4fb8fe5eb0c3
Jungwon Park <wjstk335@gmail.com>
2020-01-31 14:46:13 +0900

    takeoff

--- include/mavswarm_client/client.h
+++ include/mavswarm_client/client.h
@@ -14,6 +14,10 @@
 #include <map>
 #include <chrono>
 
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+
 #define ENABLE_SAFELINK 1
 
 class Client {
@@ -24,10 +28,18 @@ public:
 private:
     int m_mavId;
 
-    // ros
     ros::NodeHandle m_rosNodeHandle;
     ros::Publisher m_pub_externalPose;
+    ros::Publisher m_pub_setpoint;
+    ros::Subscriber m_sub_current_state;
+    ros::ServiceClient m_arming_client;
+    ros::ServiceClient m_set_mode_client;
+
+    mavros_msgs::State m_current_state;
     std::string m_frame_id;
+
+    geometry_msgs::PoseStamped m_setpoint_msgs;
+    geometry_msgs::PoseStamped m_extPose_msgs;
 
     // crazyradio
     Crazyradio* m_radio;
@@ -36,11 +48,13 @@ private:
     uint8_t m_channel;
     uint64_t m_address;
     Crazyradio::Datarate m_datarate;
-    bool m_isBroadcast;
+    bool m_is_extPose_received;
     int m_broadcast_fail_count;
 
     void handleData(const uint8_t* data);
     void publishExternalPose(const uint8_t* data);
+    void takeoff(const uint8_t* data);
+    void mavros_state_callback(const mavros_msgs::State::ConstPtr& msg);
     void quatextract(uint32_t quat, float* q);
 };
 
--- src/client.cpp
+++ src/client.cpp
@@ -17,13 +17,6 @@
 #include <iostream>
 
 const static int MAX_RADIOS = 1;
-const static bool LOG_COMMUNICATION = 0;
-
-//Crazyradio* g_crazyradios[MAX_RADIOS];
-//std::mutex g_radioMutex[MAX_RADIOS];
-//
-//CrazyflieUSB* g_crazyflieUSB[MAX_USB];
-//std::mutex g_crazyflieusbMutex[MAX_USB];
 
 Client::Client(
         const std::string& link_uri,
@@ -39,6 +32,9 @@ Client::Client(
         , m_frame_id(frame_id)
 {
     m_pub_externalPose = m_rosNodeHandle.advertise<geometry_msgs::PoseStamped>("mavswarm_client/pose", 10);
+    m_sub_current_state = m_rosNodeHandle.subscribe<mavros_msgs::State>("mavros/state", 10, &Client::mavros_state_callback, this);
+    m_arming_client = m_rosNodeHandle.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    m_set_mode_client = m_rosNodeHandle.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
     int datarate;
     int channel;
@@ -71,33 +67,38 @@ Client::Client(
 
         {
             m_radio = new Crazyradio(m_devId);
-            m_radio->setAckEnable(false);
+            m_radio->setAckEnable(true);
             m_radio->setArc(0);
             m_radio->setChannel(m_channel);
-            m_radio->setAddress(m_address);
-            m_radio->setAddr2(0xFFE7E7E7E7);
+            m_radio->setAddress(m_address); // mav address
+            m_radio->setAddr2(0xFFE7E7E7E7); // broadcast address
             m_radio->setMode(m_radio->Mode_PRX);
             m_radio->setDatarate(m_datarate);
         }
     }
 
+    m_is_extPose_received = false;
     m_broadcast_fail_count = 0;
-    m_isBroadcast = false;
 }
 
 void Client::run() {
     uint32_t length;
     uint8_t data[32];
 
-    ROS_INFO("Start mavswarm_client");
+    ROS_INFO("[MAVSWARM_CLIENT] Start mavswarm_client");
     while(ros::ok()){
         if (!m_radio->receivePacket(data, length)) {
-            ROS_INFO("ping failed");
-            continue;
+            ROS_WARN("[MAVSWARM_CLIENT] ping failed");
+            m_is_extPose_received = false;
+            m_broadcast_fail_count++;
+        }
+        else{
+            m_is_extPose_received = true;
+            handleData(data);
         }
 
-        handleData(data);
-////        ros::spinOnce();
+        m_pub_setpoint.publish(m_setpoint_msgs);
+        ros::spinOnce();
     }
 }
 
@@ -123,10 +124,35 @@ void Client::handleData(const uint8_t* data){
         ack_size = 4;
     }
     // request memory
-    else if(crtp(data[0]) == crtp(4, 0)){
+    else if(crtp(data[0]) == crtp(4, 0)) {
         ack[0] = 0x40;
         ack[1] = 0x01;
         ack_size = 3;
+    }
+    // High level setpoint - take off
+    else if(crtp(data[0]) == crtp(8, 0) && data[1] == 1){
+        takeoff(data);
+        return; //TODO: ack??
+    }
+    // High level setpoint - land
+    else if(crtp(data[0]) == crtp(8, 0) && data[1] == 2){
+
+    }
+    // High level setpoint - stop
+    else if(crtp(data[0]) == crtp(8, 0) && data[1] == 3){
+
+    }
+    // High level setpoint - goto
+    else if(crtp(data[0]) == crtp(8, 0) && data[1] == 4){
+
+    }
+    // High level setpoint - start trajectory
+    else if(crtp(data[0]) == crtp(8, 0) && data[1] == 5){
+
+    }
+    else{
+        ROS_ERROR("[MAVSWARM_CLIENT] unsupported crtp message");
+        return;
     }
     m_radio->sendPacketNoAck((uint8_t*)&ack, ack_size);
 }
@@ -135,24 +161,68 @@ void Client::publishExternalPose(const uint8_t* data) {
     auto extPose_crtp = (crtpExternalPosePacked*)data;
     for(int i = 0; i < 2; i++) {
         if (m_mavId == extPose_crtp->poses[i].id) {
-            geometry_msgs::PoseStamped extPose_msgs;
-            extPose_msgs.header.frame_id = m_frame_id;
-            extPose_msgs.header.stamp = ros::Time::now();
+            m_extPose_msgs.header.frame_id = m_frame_id;
+            m_extPose_msgs.header.stamp = ros::Time::now();
 
-            extPose_msgs.pose.position.x = (float)extPose_crtp->poses[i].x/1000;
-            extPose_msgs.pose.position.y = (float)extPose_crtp->poses[i].y/1000;
-            extPose_msgs.pose.position.z = (float)extPose_crtp->poses[i].z/1000;
+            m_extPose_msgs.pose.position.x = (float)extPose_crtp->poses[i].x/1000;
+            m_extPose_msgs.pose.position.y = (float)extPose_crtp->poses[i].y/1000;
+            m_extPose_msgs.pose.position.z = (float)extPose_crtp->poses[i].z/1000;
 
             float q[4];
             quatextract(extPose_crtp->poses[i].quat, q);
-            extPose_msgs.pose.orientation.x = q[0];
-            extPose_msgs.pose.orientation.y = q[1];
-            extPose_msgs.pose.orientation.z = q[2];
-            extPose_msgs.pose.orientation.w = q[3];
+            m_extPose_msgs.pose.orientation.x = q[0];
+            m_extPose_msgs.pose.orientation.y = q[1];
+            m_extPose_msgs.pose.orientation.z = q[2];
+            m_extPose_msgs.pose.orientation.w = q[3];
 
-            m_pub_externalPose.publish(extPose_msgs);
+            m_pub_externalPose.publish(m_extPose_msgs);
         }
     }
+}
+
+void Client::takeoff(const uint8_t* data) {
+    auto cmd_takeoff_crtp = (crtpCommanderHighLevelTakeoffRequest*)data;
+
+    if(!m_is_extPose_received){
+        ROS_WARN("[MAVSWARM_CLIENT] No external pose, do not takeoff");
+        return;
+    }
+    // initialize setpoint
+    m_setpoint_msgs = m_extPose_msgs;
+    m_setpoint_msgs.pose.position.z = cmd_takeoff_crtp->height;
+    ROS_INFO_STREAM("Initilize setpoint, x: " << m_setpoint_msgs.pose.position.x
+                                   << ", y: " << m_setpoint_msgs.pose.position.y
+                                   << ", z: " << m_setpoint_msgs.pose.position.z);
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    if( m_set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent){
+        ROS_INFO("[MAVSWARM_CLIENT] Offboard enabled");
+    }
+    else{
+        ROS_ERROR("[MAVSWARM_CLIENT] Offboard failed");
+        return;
+    }
+
+    if( !m_current_state.armed ){
+        if( m_arming_client.call(arm_cmd) && arm_cmd.response.success){
+            ROS_INFO("[MAVSWARM_CLIENT] Vehicle armed");
+        }
+        else{
+            ROS_ERROR("[MAVSWARM_CLIENT] Arming failed");
+            return;
+        }
+    }
+
+
+}
+
+void Client::mavros_state_callback(const mavros_msgs::State::ConstPtr& msg){
+    m_current_state = *msg;
 }
 
 void Client::quatextract(uint32_t quat, float* q){
